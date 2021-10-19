b0b83adeb6c38e94ca545a83ffaafc1e545d4ef2
Steven Wu <master_msp1@hotmail.com>
2021-10-19 18:13:46 +0200

    added reverse version for text-image

--- components/TextImage.vue
+++ components/TextImage.vue
@@ -4,7 +4,7 @@
     class="text-image"
   >
     <article class="container container--inner text-image__article">
-      <div class="text-image__content">
+      <div class="text-image__content" :class="{ 'text-image__content--reverse' : blok.reverse }">
         <div class="text-image__copy">
           <component
             v-for="blok in blok.copy"
@@ -13,9 +13,9 @@
             :is="blok.component"
           />
         </div>
-        <div class="text-image__image">
+        <div class="text-image__image" v-if="blok.image">
           <figure class="text-image__figure">
-            <SlantedImage class="text-image__src" url="https://images.unsplash.com/photo-1634607416888-1912b1256d5d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1941&q=80" />
+            <SlantedImage :reverse="blok.reverse" class="text-image__src" :url="blok.image.filename" />
           </figure>
         </div>
       </div>
