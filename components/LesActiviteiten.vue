f3bfe20595914c158d0316ffabdcfb6c56ec4e09
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2021-11-02 17:10:58 +0100

    styling for header lesmaterial

--- components/LesActiviteiten.vue
+++ components/LesActiviteiten.vue
@@ -4,16 +4,16 @@
     class="lessons"
   >
     <article class="container container--inner lessons__article">
-      <aside>
+      <header class="lessons__header">
         <h2 class="lessons__title">{{ blok.title }}</h2>
         <div class="lessons__filters">
           <div class="input__group">
             <input class="input input--icon" type="text" placeholder="Zoeken..." name="query"/>
             <button type="submit" class="button button--input"><Resources type="search" /></button>
           </div>
-          <DropdownSelect default="I dont care" :options="['Audi', 'BMW', 'Citroen', 'Ford']" />
+          <DropdownSelect default="Kies een categorie" :options="['Audi', 'BMW', 'Citroen', 'Ford']" />
         </div>
-      </aside>
+      </header>
       <LesActiviteit :blok="lesson.content" v-for="lesson in lessons" :key="lesson.uuid" />
     </article>
   </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,7 @@ module.exports = {
       50: "50px",
       55: "55px",
       60: "60px",
+      65: "65px",
       70: "70px",
       80: "80px",
       85: "85px",
@@ -60,6 +61,7 @@ module.exports = {
       'container': "theme(container.padding.DEFAULT)",
       'container-xl': "theme(container.padding.xl)",
       '48%': '48%',
+      '49%': '49%',
     },
     extend: {
       colors: {
@@ -135,6 +137,7 @@ module.exports = {
         '48%': '48%',
         '49%': '49%',
         'game-image': '178px',
+        'input-filter': '290px',
       },
       maxWidth: {
         'text': '690px',
