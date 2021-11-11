7f7f0787813727a866d2f27a3a3817efc7e20f35
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2021-11-11 13:11:28 +0100

    testing forms

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
+  
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
